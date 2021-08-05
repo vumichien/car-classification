1a1c90506b76be7e3f38d4378f9d34ceb1580cd1
vumichien <31467068+vumichien@users.noreply.github.com>
2021-08-05 19:13:09 +0900

    upload

---
+++ app_car_classi.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+from utils import *
+import shutil
+import time, os, json, torch, onnx, onnxruntime
+from PIL import Image
+import pandas as pd
+#import cv2
+from collections import deque, Counter
+import args
+
+st.set_page_config(
+    page_title=args.PAGE_TITLE,
+    page_icon=args.PAGE_ICON, layout=args.LAYOUT, initial_sidebar_state='auto'
+)
+
+st.title(args.LANDINGPAGE_TITLE)
+st.sidebar.title(args.SIDEBAR_TITLE)
+method = st.sidebar.radio('Choose input source ->', options=['Webcam', 'Image'])
+
+# Load model
+df_train = pd.read_csv('full_set.csv')
+sub_test_list = list(df_train['Image'].map(lambda x: get_image(x)))
+embeddings = torch.load('embeddings.pt')
+PATH = 'model_onnx.onnx'
+ort_session = onnxruntime.InferenceSession(PATH)
+input_name = ort_session.get_inputs()[0].name
+
+if method == 'Image':
+    st.sidebar.header('Options')
+    content_file, col2 = show_original()
+    image_input(
+        content_file, df_train, sub_test_list, embeddings, ort_session, input_name, col2
+    )
+else:
+    webcam_input(
+        df_train, sub_test_list, embeddings, ort_session, input_name
+    )
---
+++ args.py
@@ -0,0 +1,20 @@
+PAGE_TITLE = 'Car part Classifier'
+PAGE_ICON = "🚀"
+LAYOUT = "wide"
+
+LANDINGPAGE_TITLE = 'Car part Classification'
+SIDEBAR_TITLE = 'Navigation'
+
+
+CONTENT_IMAGES_FILE = [
+    'tail_light.jpg', 'car_wheel.jpg', 'driving_wheel.jpg',
+    'head_light.jpg', 'front_mirror.jpg', 'side_mirror.jpg',
+]
+CONTENT_IMAGES_NAME = [
+    'Tail light', 'Wheel', 'Driving wheel',
+    'Head light', 'Front mirror', 'Side mirror',
+]
+
+IMAGES_PATH = 'images'
+
+DEFAULT_CONFIDENCE_THRESHOLD = 0.8

5adad1b102f85e7b5d905044e1801afb7d85fc7d
ChienVM <vumichien1692@gmail.com>
2021-08-29 17:19:12 +0900

    add animation image

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,33 @@
-
-#import shutil
-#import cv2
-#from PIL import Image
-#from collections import deque, Counter
+# import shutil
+# import cv2
+# from PIL import Image
+# from collections import deque, Counter
 
 import time, os, json, onnx, onnxruntime
-#import torch
+# import torch
 import pandas as pd
 import streamlit as st
-
+import requests
 from utils import *
 import args
-
+from streamlit_lottie import st_lottie
 
 st.set_page_config(
     page_title=args.PAGE_TITLE,
     page_icon=args.PAGE_ICON, layout=args.LAYOUT, initial_sidebar_state='auto'
 )
+
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+lottie_penguin = load_lottieurl('https://assets10.lottiefiles.com/datafiles/Yv8B88Go8kHRZ5T/data.json')
+st_lottie(lottie_penguin, height=200)
+
 hide_streamlit_style = """
     <style>
     footer {
@@ -39,6 +50,7 @@ st.title(args.LANDINGPAGE_TITLE)
 st.sidebar.title(args.SIDEBAR_TITLE)
 method = st.sidebar.radio('Choose input source 👇', options=['Image', 'Webcam'])
 
+
 # Load model
 @st.cache(suppress_st_warning=False)
 def initial_setup():
@@ -51,6 +63,7 @@ def initial_setup():
     ort_session = onnxruntime.InferenceSession(PATH)
     input_name = ort_session.get_inputs()[0].name
     return df_train, sub_test_list, embeddings, ort_session, input_name
+
 
 df_train, sub_test_list, embeddings, ort_session, input_name = initial_setup()
 
